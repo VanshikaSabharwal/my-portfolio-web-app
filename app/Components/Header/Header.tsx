e13dd1b1b53eeabbcc6cd4d44e0bba3b7c23f9ff
Vanshika Sabharwal <vanshikasabharwal06@gmail.com>
2024-07-27 08:48:39 +0530

    added all work

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,13 +1,8 @@
 "use client";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { useContext } from "react";
-import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
-import ThemeContext from "@/app/context/ThemeContext";
-import Image from "next/image";
 
 const Header = () => {
-  const { darkTheme, setDarkTheme } = useContext(ThemeContext);
   return (
     <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nonwrap items-center justify-between">
       <div className="portfolioTitle">PortFolio</div>
@@ -42,21 +37,6 @@ const Header = () => {
         >
           Contact
         </Link>
-        <div className="flex items-center justify-center w-full md:w-1/3">
-          <button
-            aria-label="Toggle theme"
-            className="cursor-pointer icon-width-height"
-            onClick={() => {
-              setDarkTheme(!darkTheme);
-              localStorage.setItem(
-                "portfolio-theme",
-                darkTheme ? "false" : "true"
-              );
-            }}
-          >
-            {darkTheme ? <MdOutlineLightMode /> : <MdDarkMode />}
-          </button>
-        </div>
       </div>
     </header>
   );
