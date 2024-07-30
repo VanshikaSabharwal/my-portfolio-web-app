8adfffbc0e8d05878eecd7aae5fa29cbb0c52c05
Vanshika Sabharwal <vanshikasabharwal06@gmail.com>
2024-07-30 23:10:41 +0530

    styling

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -5,19 +5,17 @@ import { IoReorderThreeSharp } from "react-icons/io5";
 import { IoCloseSharp } from "react-icons/io5";
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [navMenu, setNavMenu] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(true);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
-    setNavMenu(!navMenu);
   };
   const HorizontalNav = () => {
     return (
       <div
         className={`${
           menuOpen ? "block" : "hidden"
-        }md:flex items-center flex-wrap w-full md:2/3 linkContainer`}
+        } md:flex items-center flex-wrap w-full md:2/3 linkContainer`}
       >
         <Link
           href="/"
@@ -57,7 +55,7 @@ const Header = () => {
       <div
         className={`${
           menuOpen ? "block" : "hidden"
-        }md:flex linkContainerVertical items-center flex-wrap w-full flex flex-col md:2/3 `}
+        } md:flex linkContainerVertical items-center flex-wrap w-full flex flex-col md:2/3 `}
       >
         <Link
           href="/"
@@ -96,7 +94,7 @@ const Header = () => {
   return (
     <header className="py-10 bg-black px-4 container mx-auto text-xl flex flex-wrap items-center justify-between headerContainer">
       <div className="portfolioTitle py-2.5 px-3">PortFolio</div>
-      <div className="flex items-center md:hidden">
+      <div className="flex flex-row items-center md:hidden">
         <button onClick={toggleMenu} className="text-3xl text-white">
           {menuOpen ? <IoReorderThreeSharp /> : <IoCloseSharp />}
         </button>
