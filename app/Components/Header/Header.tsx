91b3212c30d2de9193c6bf64907cf7f868eced3a
Vanshika Sabharwal <vanshikasabharwal06@gmail.com>
2024-07-30 22:23:34 +0530

    added navbar responsiveness

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -10,17 +10,10 @@ const Header = () => {
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
+    setNavMenu(!navMenu);
   };
-
-  return (
-    <header className="py-10 bg-black px-4 container mx-auto text-xl flex flex-wrap items-center justify-between headerContainer">
-      <div className="portfolioTitle py-2.5 px-3">PortFolio</div>
-      <div className="flex items-center md:hidden">
-        <button onClick={toggleMenu} className="text-3xl">
-          {menuOpen ? <IoCloseSharp /> : <IoReorderThreeSharp />}
-        </button>
-      </div>
-
+  const HorizontalNav = () => {
+    return (
       <div
         className={`${
           menuOpen ? "block" : "hidden"
@@ -57,6 +50,59 @@ const Header = () => {
           Contact
         </Link>
       </div>
+    );
+  };
+  const VerticalNav = () => {
+    return (
+      <div
+        className={`${
+          menuOpen ? "block" : "hidden"
+        }md:flex linkContainerVertical items-center flex-wrap w-full flex flex-col md:2/3 `}
+      >
+        <Link
+          href="/"
+          className="hover:-translate-y-2 duration-500 transition-all font-black text-tertiary-dark"
+        >
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className="hover:-translate-y-2 duration-500 transition-all font-black text-tertiary-dark"
+        >
+          About
+        </Link>
+        <Link
+          href="/projects"
+          className="hover:-translate-y-2 duration-500 transition-all font-black text-tertiary-dark"
+        >
+          Projects
+        </Link>
+        <Link
+          href="/services"
+          className="hover:-translate-y-2 duration-500 transition-all font-black text-tertiary-dark"
+        >
+          Services
+        </Link>
+        <Link
+          href="/contact"
+          className="hover:-translate-y-2 duration-500 transition-all font-black text-tertiary-dark"
+        >
+          Contact
+        </Link>
+      </div>
+    );
+  };
+
+  return (
+    <header className="py-10 bg-black px-4 container mx-auto text-xl flex flex-wrap items-center justify-between headerContainer">
+      <div className="portfolioTitle py-2.5 px-3">PortFolio</div>
+      <div className="flex items-center md:hidden">
+        <button onClick={toggleMenu} className="text-3xl text-white">
+          {menuOpen ? <IoReorderThreeSharp /> : <IoCloseSharp />}
+        </button>
+      </div>
+
+      {menuOpen ? <HorizontalNav /> : <VerticalNav />}
     </header>
   );
 };
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import Image from "next/image";
 import { FaLinkedin } from "react-icons/fa";
 import { RiTwitterXFill } from "react-icons/ri";
+import { IoLogoGithub } from "react-icons/io";
+import { FaHackerrank } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
 
 const About = () => {
   return (
     <div className="flex flex-col">
       <div className="aboutContainer items-center justify-center md:1/2 bg-about ">
-        <div className="grid grid-rows-1 grid-cols-2 xs:grid-cols-none xs:flex xs:grid-rows-none items-center justify-center">
+        <div className="grid grid-rows-1 grid-cols-2 xs:grid-cols-none xs:flex md:flex xs:grid-none xs:grid-rows-none items-center justify-center aboutFlex">
           <div className="bgContent">
             <h1 className="text-3xl font-bold text-black">About Me</h1>
             <p className="text-black">
@@ -29,7 +32,7 @@ const About = () => {
         </div>
         <ul className="flex flex-col items-center justify-center about-contact-section">
           <li>
-            <a href="mailto:vanshikasabharwal03@gmail" className="text-sm">
+            <a href="mailto:vanshikasabharwal03@gmail" className="text-xl">
               vanshikasabharwal03@gmail.com
             </a>
           </li>
@@ -42,6 +45,21 @@ const About = () => {
             <li>
               <a href="https://x.com/Vanshika_0006">
                 <RiTwitterXFill />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/VanshikaSabharwal">
+                <IoLogoGithub />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.hackerrank.com/profile/vanshikasabharw1">
+                <FaHackerrank />
+              </a>
+            </li>
+            <li>
+              <a href="https://leetcode.com/u/0rxMRmM6UV/">
+                <SiLeetcode />
               </a>
             </li>
           </div>
