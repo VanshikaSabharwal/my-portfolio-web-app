60fce977a23ec90a369fce3b1d2d49c12303ab98
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2025-01-07 10:42:41 +0530

    added link to intro btn

--- app/Components/Intro/Intro.tsx
+++ app/Components/Intro/Intro.tsx
@@ -7,7 +7,7 @@ const Intro = () => {
       {/* Image Section */}
       <div className="image mb-8 sm:mb-10">
         <Image
-          src="/images/vImg.png" // Replace with your image path
+          src="/images/vImg.png"
           width={150}
           height={150}
           alt="Profile Image"
@@ -25,13 +25,13 @@ const Intro = () => {
         {/* Button Section */}
         <div className="flex space-x-6 justify-center mb-4">
           <Link
-            href="/"
+            href="/contact"
             className="px-8 py-3 bg-pink-600 text-white rounded-md hover:bg-pink-700 transition duration-300"
           >
             Hire Me
           </Link>
           <Link
-            href="/"
+            href="/contact"
             className="px-8 py-3 bg-gray-800 text-white rounded-md hover:bg-gray-900 transition duration-300"
           >
             Download CV
